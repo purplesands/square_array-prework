805be095999bfae230799662e85bf673500d228f
Joe Mullen <mullenjoe13@gmail.com>
2019-01-05 22:17:57 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -7,12 +7,3 @@ def square_array(array)
 end
 return squared_array
 end
-
-def square_array(array)
-  squared_array = []
-  array.each do |num|
-    square = num * num
-    squared_array.push(square)
-  end
-  return squared_array
-end
